b321cdd989c02e7ddc7ab8ee57e6b724dc5818db
Keith Dluu <dangkhoa240899@gmail.com>
2021-05-09 10:29:33 +0700

    Enable service discovery and externalized config

--- src/main/java/com/filmbooking/api_gateway/ApiGatewayApplication.java
+++ src/main/java/com/filmbooking/api_gateway/ApiGatewayApplication.java
@@ -2,25 +2,14 @@ package com.filmbooking.api_gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.gateway.route.RouteLocator;
-import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
-import org.springframework.context.annotation.Bean;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+@EnableEurekaClient
 @SpringBootApplication
 public class ApiGatewayApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ApiGatewayApplication.class, args);
-	}
-
-	@Bean
-	public RouteLocator myRoutes(RouteLocatorBuilder builder) {
-		return builder.routes()
-			.route(p -> p
-				.path("/get")
-				.filters(f -> f.addRequestHeader("Hello", "World"))
-				.uri("http://httpbin.org:80"))
-			.build();
 	}
 
 }
