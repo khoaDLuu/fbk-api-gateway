172fce734743e220e5d1f9a4a48c2f866ab234d8
Keith Dluu <dangkhoa240899@gmail.com>
2021-05-15 20:11:35 +0700

    Update service routes

--- src/main/java/com/filmbooking/api_gateway/config/GatewayConfig.java
+++ src/main/java/com/filmbooking/api_gateway/config/GatewayConfig.java
@@ -17,25 +17,39 @@ public class GatewayConfig {
     @Bean
     public RouteLocator routes(RouteLocatorBuilder builder) {
         return builder.routes()
+                // user service
                 .route("user-service", r -> r.path("/users/**")
                         .filters(f -> f.filter(filter))
                         .uri("lb://user-service"))
 
+                // auth service
                 .route("auth-service", r -> r.path("/auth/**")
                         .filters(f -> f.filter(filter))
                         .uri("lb://auth-service"))
 
+                // booking/payment service
                 .route("booking-service", r -> r.path("/bookings/**")
                         .filters(f -> f.filter(filter))
                         .uri("https://fbooking-service.herokuapp.com"))
+
                 .route("booking-service", r -> r.path("/tickets/**")
                         .filters(f -> f.filter(filter))
                         .uri("https://fbooking-service.herokuapp.com"))
 
-                // .route("screening-service", r -> r.path("/**")
-                //         .filters(f -> f.filter(filter))
-                //         .uri("https://app-screening-service.herokuapp.com"))
+                // screening service
+                .route("screening-service", r -> r.path("/screening/**")
+                        .filters(f -> f.filter(filter))
+                        .uri("https://app-screening-service.herokuapp.com"))
 
+                .route("screening-service", r -> r.path("/date/**")
+                        .filters(f -> f.filter(filter))
+                        .uri("https://app-screening-service.herokuapp.com"))
+
+                .route("screening-service", r -> r.path("/room/**")
+                        .filters(f -> f.filter(filter))
+                        .uri("https://app-screening-service.herokuapp.com"))
+
+                // movie service
                 .route(r -> r.path("/movie/**")
                         .filters(f -> f.filter(filter))
                         .uri("https://app-movie-genre-service.herokuapp.com"))
@@ -44,6 +58,7 @@ public class GatewayConfig {
                         .filters(f -> f.filter(filter))
                         .uri("https://app-movie-genre-service.herokuapp.com"))
 
+                // test service (hhtpbin)
                 .route(r -> r.path("/get")
                         .filters(f -> f.addRequestHeader("Hello", "World"))
                         .uri("http://httpbin.org:80"))
