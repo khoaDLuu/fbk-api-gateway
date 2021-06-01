538fa5ebea53d373cac04295c3c171cc753a1d0c
Keith Dluu <dangkhoa240899@gmail.com>
2021-06-02 00:15:07 +0700

    Improve error handling for auth API call

--- src/main/java/com/filmbooking/api_gateway/config/RemoteAuth.java
+++ src/main/java/com/filmbooking/api_gateway/config/RemoteAuth.java
@@ -1,7 +1,9 @@
 package com.filmbooking.api_gateway.config;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
 import org.springframework.web.reactive.function.client.WebClient;
+import org.springframework.web.server.ResponseStatusException;
 
 import reactor.core.publisher.Mono;
 
@@ -16,10 +18,17 @@ public class RemoteAuth {
                 .header("Authorization", authValue)
                 .retrieve()
                 .toEntity(Void.class)
+                .onErrorResume(e -> Mono.error(
+                    new ResponseStatusException(
+                        HttpStatus.UNAUTHORIZED,
+                        "Your token is invalid or expired", e
+                    )
+                ))
                 .map(resp -> Boolean.valueOf(resp.getStatusCode().is2xxSuccessful()));
         }
         catch (Exception e) {
             // ############## DEBUG ############## //
+            System.out.println("[DEBUG] Exception thrown while calling API: ");
             e.printStackTrace();
             // ############## DEBUG ############## //
             return Mono.just(false);
